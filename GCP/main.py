2db35ca173996dd176ece8c9f434fec0ade2003a
xavierdebeaumont <xdebeaumont@outlook.fr>
2023-06-19 16:44:25 +0200

    added loading of 12 years of data the first time

--- GCP/main.py
+++ GCP/main.py
@@ -9,7 +9,6 @@ ls_ticker = 'AI.PA'
 yahoo_financials = YahooFinancials(ls_ticker)
 
 today = datetime.today()
-start_date = (today - timedelta(days=5)).strftime("%Y-%m-%d")  # generate daily file for last 5 days
 end_date = today.strftime("%Y-%m-%d")
 
 def upload_csv_gcs(data_dictionary, bucket_name, blob_name):
@@ -32,16 +31,34 @@ def upload_csv_gcs(data_dictionary, bucket_name, blob_name):
     blob.upload_from_string(file_buff.getvalue(), content_type='text/csv')
     print('Done uploading to GCS')
 
+def check_csv_exists(bucket_name):
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    blobs = bucket.list_blobs()
+
+    # Check if any blob has the .csv extension
+    for blob in blobs:
+        if blob.name.endswith('.csv'):
+            return True
+
+    return False
+
 def load_data(request):
+    # Get the bucket name from the environment variable
+    bucket_name = os.getenv('BUCKET_NAME')
+
+    # Check if CSV file already exists in the bucket
+    if check_csv_exists(bucket_name):
+        start_date = (today - timedelta(days=1)).strftime("%Y-%m-%d")  # generate daily file for last day
+    else: 
+        start_date = (today - timedelta(days=4668)).strftime("%Y-%m-%d")  # generate daily file for last 12 years
+
     stock_prices = yahoo_financials.get_historical_price_data(start_date, end_date, 'daily')
     table_data = stock_prices[ls_ticker]['prices']
     for l in table_data:
         l.pop('date')
         l['ticker'] = ls_ticker
     table_rows = len(table_data)
-
-    # Get the bucket name from the environment variable
-    bucket_name = os.getenv('BUCKET_NAME')
 
     # Create CSV and upload to GCS bucket
     dt_string = datetime.now().strftime("%Y-%m-%d_%H%M")
